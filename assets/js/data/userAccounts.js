9c99bd69dae1bc730ea3f90b57ef30bb7fd3401a
Haneen-Radad <160304061+Haneen-Radad@users.noreply.github.com>
2024-12-31 17:32:57 +0200

    Remove old Phase 1 code

--- assets/js/data/userAccounts.js
+++
@@ -1,9 +0,0 @@
-// Load users from localStorage or use the default array if none exist
-let users = JSON.parse(localStorage.getItem("users")) || [
-    { username: "admin1", password: "admin123", fullName: "Admin One", role: "admin" },
-    { username: "admin2", password: "admin456", fullName: "Admin Two", role: "admin" },
-    { username: "admin3", password: "admin789", fullName: "Admin Three", role: "admin" },
-];
-
-// Export the array to make it accessible in other files
-export default users;
